b41a3b8010e51dde9b01e3c349ff9d5778376e76
ValentinAntonioFlores <Vflores@mail.austral.edu.ar>
2025-10-02 20:32:08 -0300

    Initial Spring Boot project

---
+++ src/main/kotlin/com/ingsis/grupo10/auth/AuthApplication.kt
@@ -0,0 +1,11 @@
+package com.ingsis.grupo10.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthApplication>(*args)
+}
